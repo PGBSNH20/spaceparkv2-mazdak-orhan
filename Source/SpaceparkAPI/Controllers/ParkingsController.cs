9e509a321e61251ecff7b08270df43b0c3b51492
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-04-30 15:23:51 +0200

    Added more base files

---
+++ Source/SpaceparkAPI/Controllers/ParkingsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpaceParkAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceparkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParkingsController : ControllerBase
+    {
+        private SpaceParkContext _dbContext;
+        public ParkingsController(SpaceParkContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //[HttpPost]
+        //public IActionResult Post
+    }
+}
