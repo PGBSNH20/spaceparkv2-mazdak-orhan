c1839d92565df50c197a395f9be31ae0948181de
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-05-03 17:59:06 +0200

    Added Put,Get,Post for SpacePorts

--- Source/SpaceparkAPI/Controllers/SpaceportController.cs
+++ Source/SpaceparkAPI/Controllers/SpaceportController.cs
@@ -19,24 +19,71 @@ namespace SpaceparkAPI.Controllers
             _dbContext = dbContext;
         }
 
-        [HttpPost("[action]")]
-        public IActionResult AddNewSpacePort([FromForm]SpacePort spaceportObj)
+        [HttpPost("[action]/{name}")]
+        public IActionResult AddNewSpacePort(string name)
         {
-            _dbContext.Add(spaceportObj);
+            var spacePort = new SpacePort()
+            {
+                Name = name
+            };
+
+            var findExistingSpacePort = _dbContext.SpacePorts.Any(x => x.Name == name);
+            if (findExistingSpacePort)
+            {
+                return BadRequest($"There is already and existing spaceport named: {name}, try with another one.");
+            }
+
+            _dbContext.Add(spacePort);
             _dbContext.SaveChanges();
-            return StatusCode(StatusCodes.Status201Created);
+            return Ok($"'{name}' spaceport has been created");
         }
 
         [HttpGet("[action]")]
-        public IActionResult GetSpacePorts()
+        public IActionResult GetAllSpacePorts()
         {
             var spaceports = from spaceport in _dbContext.SpacePorts
-                           select new
-                           {
-                               Id = spaceport.Id,
-                               Name = spaceport.Name
-                           };
+                             select new
+                             {
+                                 Id = spaceport.Id,
+                                 Name = spaceport.Name
+                             };
             return Ok(spaceports);
+        }
+
+        [HttpGet("[action]/{name}")]
+        public IActionResult GetSpacePortByName(string name)
+        {
+            var findExistingSpacePort = _dbContext.SpacePorts.SingleOrDefault(x => x.Name == name);
+            if (_dbContext.SpacePorts == null || findExistingSpacePort == null)
+            {
+                return BadRequest("We can't find the requested spaceport.");
+            }
+
+            var spaceports = (from spaceport in _dbContext.SpacePorts where spaceport.Name == name
+                             select new
+                             {
+                                 Id = spaceport.Id,
+                                 Name = spaceport.Name
+                             }).FirstOrDefault();
+            return Ok(spaceports);
+        }
+
+        [HttpPut("[action]/{name}")]
+        public IActionResult UpdateSpacePort(string name, string newName)
+        {
+            var findSpacePort = _dbContext.SpacePorts.SingleOrDefault(x => x.Name == name);
+
+            if (findSpacePort != null)
+            {
+                findSpacePort.Name = newName;
+                _dbContext.SaveChanges();
+                return Ok($"Spaceport name updated to {newName}");
+            }
+            else
+            {
+                return NotFound($"We can't find any spaceports named: {name}");
+            }
+
         }
     }
 }
