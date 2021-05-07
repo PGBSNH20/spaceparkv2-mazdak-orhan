f4d40737e161a381ba69c3a0cd5e1d870093bf08
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-05-07 19:17:00 +0200

    Console App open for further development in project

--- SpaceParkConsoleApp/Program.cs
+++ SpaceParkConsoleApp/Program.cs
@@ -9,35 +9,14 @@ namespace SpaceParkConsoleApp
 {
     class Program
     {
-        private static string APIUrl = "http://localhost:44382/api/";
+        //private static string APIUrl = "http://localhost:44382/api/";
 
         static void Main(string[] args)
         {
             //Added this line to Parse double values to not mix "." and ","
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
-            Console.WriteLine("Welcome dear traveller!\n");
-
-            
-        }
-
-        public static async void GetDataWithoutAuthentication()
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(APIUrl);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage response = await client.GetAsync(APIUrl);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var readTask = response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    var rawResponse = readTask.GetAwaiter().GetResult();
-                    Console.WriteLine(rawResponse);
-                }
-                Console.WriteLine("Complete");
-            }
+            //Code below to work with SpaceparkAPI project inside console app.          
         }
     }
 }
